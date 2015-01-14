8804738073a8b4ebb37fdbfe320a6a65ef3f7837
TimmyTF <timmytf@gmail.com>
2015-01-14 11:04:31 +0300

    An item added to the class context menu. ComparisonChainGen dialog added.

---
+++ src/org/jetbrains/comparisonChainGen/GenerateAction.java
@@ -0,0 +1,43 @@
+package org.jetbrains.comparisonChainGen;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+
+/**
+ * Created by Timmy on 13.08.2014.
+ *
+ */
+public class GenerateAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        PsiClass psiClass = getPsiClassFromContext(e);
+        GenerateDialog dlg = new GenerateDialog(psiClass);
+        dlg.show();
+    }
+
+    @Override
+    public void update(AnActionEvent e) {
+        PsiClass psiClass = getPsiClassFromContext(e);
+        e.getPresentation().setEnabled(psiClass != null);
+    }
+
+    private PsiClass getPsiClassFromContext(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        if (psiFile == null || editor == null) {
+            e.getPresentation().setEnabled(false);
+            return null;
+        }
+
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAt = psiFile.findElementAt(offset);
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+        return psiClass;
+    }
+}
